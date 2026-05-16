766a6175783a52c4c780f3279dbb8314aaedea5f
JerryAkuaku <133439738+JerryAkuaku@users.noreply.github.com>
2026-05-16 19:14:01 +0000

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+       s=s.replace(/[^a-zA-Z0-9]/g, "")
+       s=s.toLowerCase()
+        let right=s.length-1
+        for(let left=0;left<Math.floor(s.length/2);left++){
+            if(s[right]!=s[left]){
+            return false
+            }
+            right--
+        }
+        return true
+    }
+}
